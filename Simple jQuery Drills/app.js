6fe2d34f1cbe0f7c55b735dbd73de89299d98730
4mpl1f13d <4mpl1f13d@users.noreply.github.com>
2020-01-28 16:43:41 -0600

    Add files via upload

--- Simple jQuery Drills/app.js
+++ Simple jQuery Drills/app.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
 
-    //Text Alert
-    $('#btnSubmit').click(function () {
-        alert("The Button was clicked!");
-        alert($('#textID').val());
-
-
-    })
-
-    //Button Enabled / Disabled
+    //Button Enabled OR Disabled via type
     $('#textID').keyup(() => {
         if ($('#textID').val() === "") {
             $('#btnSubmit').attr('disabled', true);
@@ -17,9 +9,63 @@ $(document).ready(function () {
             $('#btnSubmit').attr('disabled', false);
         }
 
+    });
+
+
+    //Text Alert
+    $('#btnSubmit').click(function () {
+        //Prevents page refresh
+        event.preventDefault();
+        // alert("The Button was clicked!");
+        // alert($('#textID').val());
+
+        // H2
+        // let insertText = $('#textID').val();
+        // let h2 = $('<h2></h2>');
+        // h2.text(insertText);
+        // $(".divClass").append(h2);
+
+        // //When the user mouses over the h2 element, assign it a new background color and border radius.
+        // $('h2').mouseover(function () {
+        //     $(this).css({
+        //         'background-color': 'darkblue',
+        //         'border-Radius ': '25px'
+        //     });
+        // });
+
+        // Use jQuery to create an unordered list and append it to the body When the user submits the form, append the text as list item to the unordered list
+
+        let colorArray = ["#C2272D", "#db7c0f", "#FFFF01", "#009245", "#91c3db", "#ca04ed", "#612F90", "#aeff00"]
+        function generateRandomColor() {
+            return Math.floor((Math.random() * 8));
+        };
+
+        let insertLi = $('#textID').val();
+        let li = $('<li></li>');
+        li.text(insertLi);
+        $(".ulClass").append(li);
+
+        $('li').click(function () {
+            let randomNumber = generateRandomColor();
+            $(this).css({
+                'color': colorArray[randomNumber]
+            });
+        });
+
+        $('li').dblclick(function () {
+            $(this).remove();
+        });
 
 
     });
+
+
+
+    // // Append div to body
+    // $("body").append($("<div class='divClass'></div>"));
+
+    // Append ul to body
+    $("body").append($("<ul class='ulClass'></ul>"));
 
 
 });
